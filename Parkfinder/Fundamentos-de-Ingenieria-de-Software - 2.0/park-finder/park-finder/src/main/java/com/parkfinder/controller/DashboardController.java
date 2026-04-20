a06fc71dd60037de7edf32afc753c549aa2c7c84
Andy <pipelin0506@gmail.com>
2026-04-20 18:27:26 -0500

    Cambios de pom y creacion de pantallas

---
+++ Parkfinder/Fundamentos-de-Ingenieria-de-Software - 2.0/park-finder/park-finder/src/main/java/com/parkfinder/controller/DashboardController.java
@@ -0,0 +1,21 @@
+package com.parkfinder.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+    public void irAReservas(MouseEvent mouseEvent) {
+    }
+
+    public void irAHistorial(MouseEvent mouseEvent) {
+    }
+
+    public void irASuscripcion(MouseEvent mouseEvent) {
+    }
+
+    public void handleSeleccionarParqueadero(MouseEvent mouseEvent) {
+    }
+
+    public void handleBuscar(ActionEvent actionEvent) {
+    }
+}
